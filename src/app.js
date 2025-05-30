935fd962cdffa91709451f6b914ab2088632c2c9
Romina Verónica Torres <110548507+RomiTorres@users.noreply.github.com>
2025-05-30 18:33:41 +0000

    Estilos css a la CArd y el interior

--- src/app.js
+++ src/app.js
@@ -7,5 +7,24 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  function generateCard() {
+    const suit = ["♦", "♥", "♠", "♣"];
+    const number = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+    const cardSuit = Math.floor(Math.random() * suit.length);
+    const randomNumber = Math.floor(Math.random() * number.length);
+  }
+
+  function cardRed() {
+    let suitCard = document.querySelectorAll(".suit");
+    suitCard.forEach(element => {
+      if (element.innerHTML === "♦" || element.innerHTML === "♥") {
+        element.style.color = "red";
+      } else {
+        element.style.color = "";
+      }
+    });
+  }
+
   console.log("Hello Rigo from the console!");
 };
