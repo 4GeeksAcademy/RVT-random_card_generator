db8382b2f39c92094c6f34aa07bdbac0f157c7fb
Romina Verónica Torres <110548507+RomiTorres@users.noreply.github.com>
2025-05-30 18:55:17 +0000

    Funcionamiento correcto

--- src/app.js
+++ src/app.js
@@ -7,24 +7,72 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  function generateCard() {
-    const suit = ["♦", "♥", "♠", "♣"];
-    const number = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  const numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
+  const suits = ["♦", "♥", "♠", "♣"];
 
-    const cardSuit = Math.floor(Math.random() * suit.length);
-    const randomNumber = Math.floor(Math.random() * number.length);
-  }
+  // const topSuit = document.querySelector("#topSuit");
+  const numb = document.querySelector("#number");
+  // const bottomSuit = document.querySelector("#bottomSuit");
+  const cardElement = document.querySelector(".card");
+  const btn = document.querySelector("#btn");
+  const countdownEl = document.querySelector("#countdown");
+  const widthInput = document.querySelector("#widthInput");
+  const heightInput = document.querySelector("#heightInput");
+  const resizeBtn = document.querySelector("#resizeBtn");
 
-  function cardRed() {
-    let suitCard = document.querySelectorAll(".suit");
-    suitCard.forEach(element => {
-      if (element.innerHTML === "♦" || element.innerHTML === "♥") {
-        element.style.color = "red";
-      } else {
-        element.style.color = "";
-      }
-    });
-  }
+
+
+
+  function toggleCard() {
+    const randomNumb = Math.floor(Math.random() * numbers.length);
+    const randomSuits = Math.floor(Math.random() * suits.length);
+    const selectedSuit = suits[randomSuits];
+
+    const suitClasses = {
+      "♦": "diamond",
+      "♥": "heart",
+      "♠": "spade",
+      "♣": "club"
+    };
+
+    topSuit.innerHTML = selectedSuit;
+    numb.innerHTML = numbers[randomNumb];
+    bottomSuit.innerHTML = selectedSuit;
+
+    cardElement.classList.remove("diamond", "heart", "spade", "club");
+    cardElement.classList.add(suitClasses[selectedSuit])
+
+    }
+
+  toggleCard();
+
+  btn.addEventListener("click", () => {
+    toggleCard();
+    countdown = 10;
+  });
+
+  let countdown = 10;
+  countdownEl.textContent = `Nueva carta en ${countdown}`;
+
+
+  setInterval(() => {
+    countdown--;
+    countdownEl.textContent = `Nueva carta en ${countdown}`;
+
+    if (countdown === 0) {
+      toggleCard();
+      countdown = 10;
+    }
+  }, 1000);
+
+  resizeBtn.addEventListener("click", () =>{
+    const width = widthInput.value;
+    const height = heightInput.value;
+
+    cardElement.style.width = `${width}px`;
+    cardElement.style.height = `${height}px`;
+
+  })
 
   console.log("Hello Rigo from the console!");
 };
